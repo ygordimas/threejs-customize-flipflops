293507adc723c7e283f3592ce7d394afdbebfbda
Ygor Dimas <ygordimas.dev@gmail.com>
2024-02-26 21:03:28 +0000

    File Picker

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
-const FilePicker = () => {
-  return <div>FilePicker</div>;
+import CustomButton from "./CustomButton";
+
+const FilePicker = ({ file, setFile, readFile }) => {
+  return (
+    <div className="filepicker-container">
+      <div className="flex flex-1 flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+
+        <p className="mt-2 truncate text-xs text-gray-600">
+          {file === "" ? "no file selected" : file.name}
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-3">
+        <CustomButton
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile("logo")}
+          customStyles="text-xs"
+        />
+        <CustomButton
+          type="filled"
+          title="Full"
+          handleClick={() => readFile("full")}
+          customStyles="text-xs"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FilePicker;
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -14,7 +14,7 @@ import {
   FilePicker,
   Tab,
 } from "../components";
-import { EditorTabs, FilterTabs } from "../config/constants";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
@@ -38,12 +38,51 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />;
       case "filepicker":
-        return <FilePicker />;
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
         return <AIPicker />;
       default:
         return null;
     }
+  };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+    }
+
+    //after setting the state, activeFilterTab is updated
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      };
+    });
+  };
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
   };
 
   return (
@@ -93,8 +132,10 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => {
+                  handleActiveFilterTab(tab.name);
+                }}
               />
             ))}
           </motion.div>
